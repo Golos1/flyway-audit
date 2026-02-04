3c8ac3fa0b85ba1a171667b10537dd8e26b1ba25
Golos1 <68024905+Golos1@users.noreply.github.com>
2026-02-04 14:16:31 -0500

    Initial commit, project will be tested in CI

---
+++ src/test/java/backup/S3PostMigrationStatementTest.java
@@ -0,0 +1,19 @@
+package backup;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class S3PostMigrationStatementTest {
+    @Test
+    public void testHandle(){
+        Flyway flyway = Flyway.configure().
+                dataSource("jdbc:h2:file:./test/dbfile", "sa", null).
+                callbacks(new S3PostMigrationStatement("TestMigrationStatements")).
+                load();
+        assertDoesNotThrow(flyway::migrate);
+    }
+
+
+}
