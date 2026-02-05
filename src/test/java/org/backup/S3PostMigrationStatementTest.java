1d27f3ca8024eb2a3aa2dbcca0621694e9a26e10
Golos1 <68024905+Golos1@users.noreply.github.com>
2026-02-05 09:59:03 -0500

    fixed testing, changed packaging to jar

--- src/test/java/org/backup/S3PostMigrationStatementTest.java
+++ src/test/java/org/backup/S3PostMigrationStatementTest.java
@@ -2,15 +2,22 @@ package org.backup;
 
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class S3PostMigrationStatementTest {
     @Test
     public void testHandle(){
+        S3PostMigrationStatement callback;
+        try {
+            callback = new S3PostMigrationStatement("test-flyway-callback");
+        }catch (Exception e){
+            fail(e);
+            return;
+        }
         Flyway flyway = Flyway.configure().
                 dataSource("jdbc:h2:file:./target/foobar", "sa", null).
-                callbacks(new S3PostMigrationStatement("test-flyway-callback")).
+                callbacks(callback).
                 locations("../../../../java/db/migration").
                 load();
         assertDoesNotThrow(flyway::migrate);
